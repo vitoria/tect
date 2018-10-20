0d3b08a93eec96ee77bdbff38e53a759d0b9787a
Vitória Heliane <vitoriaheliane@gmail.com>
2018-10-20 19:15:47 -0300

    Add test case menu

--- Haskell/src/GeneralPrints.hs
+++ Haskell/src/GeneralPrints.hs
@@ -1,12 +1,24 @@
 module GeneralPrints where
 
-import Constants
+    import System.Process
+    import Constants
 
-printHeader :: IO()
-printHeader = do
-    putStrLn header
+    printHeader :: IO()
+    printHeader = do
+        putStrLn header2
 
-printHeaderWithSubtitle :: String -> IO()
-printHeaderWithSubtitle subtitle = do
-    printHeader
-    putStrLn subtitle
+    printHeaderWithSubtitle :: String -> IO()
+    printHeaderWithSubtitle subtitle = do
+        printHeader
+        putStrLn subtitle
+
+    systemPause :: IO()
+    systemPause = do
+        putStrLn pause_msg
+        line <- getLine
+        clearScreen
+
+    clearScreen :: IO ()
+    clearScreen = do
+        _ <- system "clear"
+        return ()
--- Haskell/src/TestCase.hs
+++ Haskell/src/TestCase.hs
@@ -1,1 +1,35 @@
-module TestCase where
+import Constants
+import GeneralPrints
+-- module TestCase where
+
+isOptionValid :: Int -> Bool
+isOptionValid option = option >= 1 && option <= 6
+
+chooseProcedure :: Int -> IO()
+chooseProcedure 1 = do print "CREATE"
+chooseProcedure 2 = do print "CREATE"
+chooseProcedure 3 = do print "CREATE"
+chooseProcedure 4 = do print "CREATE"
+chooseProcedure 5 = do print "CREATE"
+chooseProcedure 6 = do print "CREATE"
+chooseProcedure option = do print "NOT CREATE"
+
+showMenu :: IO()
+showMenu = do 
+    printHeaderWithSubtitle test_case_header
+    putStrLn test_Case_Menu
+
+menu :: IO()
+menu = do
+    showMenu
+    putStrLn choose_option
+    input <- getLine
+    let option = read input :: Int
+    if isOptionValid option
+        then chooseProcedure option
+    else do
+        systemPause
+        menu
+
+main = do
+    menu
