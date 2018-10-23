91e57979a099c127ad36fdd21e73ce39fc1904ee
mateusqc <mateusqcunha@gmail.com>
2018-10-23 15:17:16 -0300

    primitive writing and reading from file functions

--- Haskell/src/TestSuite.hs
+++ Haskell/src/TestSuite.hs
@@ -2,6 +2,10 @@ import Constants
 import GeneralPrints
 import Validation
 import System.IO
+import System.IO.Unsafe
+import System.Directory
+import Data.List
+
 
 data Suite = Suite {
     suiteId :: Int,
@@ -9,6 +13,49 @@ data Suite = Suite {
     suiteDescription :: String,
     projectId :: Int
 } deriving(Eq, Show)
+
+readSuitesToStr :: Int -> IO [String]
+readSuitesToStr projId = do
+    let filePath = data_folder_path ++ "/" ++ (show projId) ++ "/" ++ suites_file_path
+    handle <- openFile filePath ReadMode
+    fileContents <- hGetContents handle
+    let contentsList = lines fileContents
+    return contentsList
+
+writeSuite :: Suite -> Int -> IO()
+writeSuite suite projId = do
+    let filePath = data_folder_path ++ "/" ++ (show projId) ++ "/" ++ suites_file_path
+    let folderPath = data_folder_path ++ "/" ++ (show projId) ++ "/"
+    if unsafePerformIO $ doesFileExist filePath
+        then do
+            handle <- openFile filePath ReadMode
+            tempdir <- getTemporaryDirectory
+            (tempName, tempHandle) <- openTempFile tempdir "temp"
+            fileContents <- hGetContents handle
+            let newSuitesStrings = fileContents ++ (suiteToString suite)
+            hPutStr tempHandle newSuitesStrings
+            hClose handle
+            hClose tempHandle
+            removeFile filePath
+            renameFile tempName filePath
+        else do
+            if unsafePerformIO $ doesDirectoryExist data_folder_path
+                then do createDirectory (data_folder_path ++ "/")
+                else do 
+                    createDirectory (data_folder_path ++ "/")
+                    createDirectory folderPath
+            --tempdir <- getTemporaryDirectory
+            --(tempName, tempHandle) <- openTempFile tempdir "temp"
+            tempHandle <- openFile filePath WriteMode
+            let newSuitesStrings = suiteToString suite
+            hPutStr tempHandle newSuitesStrings
+            hClose tempHandle
+            --renameFile tempName filePath
+
+
+
+suiteToString :: Suite -> String
+suiteToString (Suite suiteId name suiteDescription projectId) = (show suiteId) ++ "\n" ++ name ++ "\n" ++ suiteDescription ++ "\n" ++ (show projectId) ++ "\n"
 
 createSuite :: Int -> String -> String -> Int -> Suite
 createSuite idInput nameInput descriptionInput projectIdInput = Suite {suiteId = idInput,
@@ -28,8 +75,11 @@ chooseProcedure projId option
         descrInput <- getLine
         let newSuite = createSuite (read idInput) nameInput descrInput projId
         putStrLn(show newSuite)
+        putStrLn "Tentando escrever suite no arquivo..."
+        writeSuite newSuite projId
 
-    | option == list_suites = do print "LIST SUITE"
+    | option == list_suites = do
+        print "LIST SUITE"
     | option == search_suite = do print "SEARCH SUITE"
     | option == edit_suite = do print "EDIT SUITE"
     | option == delete_suite = do print "DELETE SUITE"
