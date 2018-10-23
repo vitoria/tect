8b96ee3d11526e44c4be6f4e16747c397e0196f9
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2018-10-23 11:25:27 -0300

    Leitura do arquivo parcialmente feita

--- Haskell/src/cadastro.hs
+++ Haskell/src/cadastro.hs
@@ -1,5 +1,6 @@
 import Constants
 import System.IO
+import Data.List
 
 data User = User {
     name :: String,
@@ -20,27 +21,27 @@ ddd = [dd]
 zzz = []
 -- Teste/
 
-registerNewUser :: IO User
+registerNewUser :: IO ()
 registerNewUser = do
                 putStrLn (header)
                 putStrLn (sign_up_header)
                 putStrLn ("")
                 putStr (name_const)
                 n <- getLine
-                u <-getUser
+                u <- getUser
                 putStr (password_const)
                 p <- getLine
                 verifySenha <- getPassword p
                 putStrLn ("Usuário cadastrado com sucesso!")
-                return User {name = n, username = u, password = p}
+                saveUser User {name = n, username = u, password = p}
 
 getUser :: IO String
 getUser = do 
                 putStr (username_const)
                 u <- getLine
-                if (verifyExistingUser
-                 u aaa) -- lista (aaa) utilizada como paremetro deve ser uma lista gerada a partir do arquivo com os usuários
-                    then 
+                if (verifyExistingUser u aaa) -- lista (aaa) utilizada como paremetro deve ser uma lista gerada a partir do arquivo com os usuários
+                    then do 
+                        putStr ("Username já cadastrado\n")
                         getUser
                     else 
                         return u
@@ -53,8 +54,8 @@ getPassword x = do
                 if senha == x 
                     then 
                         return senha 
-                    else 
-                        -- error "Senha não confere"
+                    else do
+                        putStr ("Senhas não confere\n")
                         getPassword x
 
 verifyExistingUser :: String -> [User] -> Bool
@@ -87,3 +88,15 @@ cleanFile a = do
                 hPutStr arq ""
                 hFlush arq
                 hClose arq
+
+readFileUsers :: IO [User]
+readFileUsers = do
+            content <- readFile "data/users.dat"
+            let listUser = content
+            let lineUser = (lines listUser)
+            return (stringsToUser lineUser)
+
+
+stringsToUser :: [String] -> [User]
+stringsToUser [] = []
+stringsToUser (x:(y:(z:xs))) = (User x y z) : stringsToUser xs
