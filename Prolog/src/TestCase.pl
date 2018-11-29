3189a8499504695597bdd2df86e444ac345054f3
LucasCBrasil <lucascordeirobr@gmail.com>
2018-11-29 01:18:13 -0300

    Adding Test cases functionalities. Still not showing the steps and also missing data persistency

---
+++ Prolog/src/TestCase.pl
@@ -0,0 +1,192 @@
+:- module(testCases, [suiteMenu/2]).
+
+:- use_module("Constants").
+/*testCase(projectId, suiteId, caseId, name, goals, status, preconditions).
+step(stepId, caseId, details, expectedResult).
+*/
+:- dynamic testCase/7.
+:- dynamic step/4.
+
+readNumber(Number):- read_line_to_codes(user_input, Codes), string_to_atom(Codes, Atom), atom_number(Atom, Number).
+
+stepCount(0).
+
+printSuiteMenu():- 
+    constants: edit_suite_header(EditSuiteHeader),
+    writeln(EditSuiteHeader),
+    constants: test_Case_Menu(TestMenu),
+    writeln(TestMenu).
+
+suiteMenu(ProjectId, SuiteId) :-
+    printSuiteMenu,
+    readNumber(Option),
+    selectOptionTestCase(Option, ProjectId, SuiteId).
+
+selectOptionTestCase(Option, ProjectId, SuiteId):-
+    constants:test_case_header(CaseHeader),
+    ((Option == 1 -> createTestCase(ProjectId, SuiteId);
+    Option == 2 -> writeln(CaseHeader), listTestCases(ProjectId, SuiteId, 1);
+    Option == 3 -> searchTestCase(ProjectId, SuiteId);
+    Option == 4 -> editTestCase(ProjectId, SuiteId);
+    Option == 5 -> deleteTestCase(ProjectId, SuiteId);
+    Option == 6 -> write(suite_menu);
+    writeln('Opcao invalida!')),
+    writeln('Pressione qualquer tecla para continuar...'),
+    get_char(_)),
+    suiteMenu(ProjectId, SuiteId).
+
+createTestCase(ProjectId, SuiteId):-
+    constants: create_case_header(CreateTestCase),
+    writeln(CreateTestCase),
+    getNumberOfTestCases(NumberOfCases),
+    NewNumberOfCases is NumberOfCases + 1,
+    createCase(ProjectId, SuiteID, NewNumberOfCases).
+
+getNumberOfTestCases(Count) :- 
+    aggregate_all(count, testCase(_, _, _, _, _, _, _), Count).
+
+createCase(ProjectId, SuiteId, CaseId) :-
+    constants:name_const(NameConst),
+    constants:goal(GoalConst),
+    constants:preconditions(PreConst),
+    constants:case_steps_reading_header(Steps),
+    writeln(NameConst),
+    read_line_to_string(user_input, Name), nl,
+    writeln(GoalConst),
+    read_line_to_string(user_input, Goal), nl,
+    writeln(PreConst),
+    read_line_to_string(user_input, Preconditions), nl,
+    assertz(testCase(ProjectId, SuiteId, CaseId, Name, Goal, 'Nao executado',  Preconditions)),
+    writeln(Steps),
+    stepCount(StepId),
+    continueSteps(ProjectId, SuiteId, CaseId, StepId).
+
+continueSteps(ProjectId, SuiteId, CaseId, StepId) :-
+    constants:case_step_description(CaseDesc),
+    constants:case_step_expected_result(CaseResult),
+    constants:case_step_continue_message(CaseContinueMsg),
+    NewStepId is StepId + 1,
+    writeln(CaseDesc),
+    read_line_to_string(user_input, Description), 
+    writeln(CaseResult),
+    read_line_to_string(user_input, ExpectedResult),
+    assertz(step(NewStepId, CaseId, Description, ExpectedResult)),
+    writeln('Deseja inserir outro passo ((1)Sim, (2)Não)?'),
+    readNumber(Option),
+    ((Option == 1) -> continueSteps(ProjectId, SuiteId, CaseId, NewStepId);
+    (Option == 2) -> writeln('Pressione qualquer tecla para continuar...'), get_char(_), suiteMenu(ProjectId, SuiteId);
+    writeln('Opcao invalida. Nenhum passo a mais será adicionado. Redirecionando para o menu da suite de testes...'),
+    writeln('Pressione qualquer tecla para continuar...'),
+    get_char(_),
+    suiteMenu(ProjectId, SuiteId)).
+
+listTestCases(ProjectId, SuiteId, CurrentCase) :-
+    getNumberOfTestCases(NumberOfCases),
+    NewNumberOfCases is NumberOfCases + 1,
+    ((CurrentCase < NewNumberOfCases) ->
+        listCase(ProjectId, SuiteId, CurrentCase),
+        NewCurrentCase is CurrentCase + 1,
+        listTestCases(ProjectId, SuiteId, NewCurrentCase));
+    writeln('Pressione qualquer tecla para continuar...'),
+    get_char(_),
+    suiteMenu(ProjectId, SuiteId).
+
+listCase(ProjectId, SuiteId, CurrentCase) :-
+    testCase(ProjectId, SuiteId, CurrentCase, Name, Goals, Status, Preconditions),
+    write('Id: '),
+    write(CurrentCase), nl,
+    write('Nome: '),
+    write(Name), nl,
+    write('Objetivos: '),
+    write(Goals), nl,
+    write('Pre-condicoes: '),
+    write(Preconditions), nl,
+    write('Status: '),
+    write(Status), nl.
+
+searchTestCase(ProjectId, SuiteId) :-
+    constants:search_case_header(Search),
+    writeln(Search),
+    writeln('Informe o id do Caso de Teste: '),
+    readNumber(CaseId),
+    ((validCaseId(CaseId) ->
+        listCase(ProjectId, SuiteId, CaseId));
+    (write('Id nao cadastrado para casos de teste.'), nl)),
+    writeln('Pressione qualquer tecla para continuar...'),
+    get_char(_),
+    suiteMenu(ProjectId, SuiteId).
+
+validCaseId(Id) :-
+    getNumberOfTestCases(NumberOfCases),
+    NewNumberOfCases is NumberOfCases + 1,
+    Id < NewNumberOfCases.
+
+editTestCase(ProjectId, SuiteId) :-
+    constants:edit_case_header(CaseHeader),
+    writeln(CaseHeader),
+    writeln('Informe o id do Caso de Teste: '),
+    readNumber(CaseId), nl,
+    (validCaseId(CaseId) -> menuEditCase(ProjectId, SuiteId, CaseId));
+    (writeln('Id nao cadastrado para casos de teste.'),
+    writeln('Pressione qualquer tecla para continuar...'),
+    get_char(_),
+    suiteMenu(ProjectId, SuiteId)).
+
+overwriteCase(ProjectId, SuiteId, CaseId):-
+    testCase(ProjectId, SuiteId, CaseId, Name, Goal, Status, Preconditions),
+    retract(testCase(ProjectId, SuiteId, CaseId, Name, Goal, Status, Preconditions)),
+    createCase(ProjectId, SuiteId, CaseId).
+
+menuEditCase(ProjectId, SuiteId, CaseId) :-
+    constants:edit_case_header(CaseHeader),
+    writeln(CaseHeader),
+    writeln('(1) Editar Dados do Caso de Teste'),
+    writeln('(2) Editar Status do Caso de Teste'),
+    writeln('(3) Voltar'),
+    read_line_to_string(user_input, Selected), nl,
+    atom_number(Selected, Option),
+    ((Option == 1 -> write('Editando Caso de Testes'), nl,
+    writeln('Estado atual'),
+    listCase(ProjectId, SuiteId, CaseId), nl,
+    writeln('Estado novo'),
+    overwriteCase(ProjectId, SuiteId, CaseId),
+    listCase(ProjectId, SuiteId, CaseId), nl);
+    (Option == 2 -> menuChangeStatus(ProjectId, SuiteId, CaseId));
+    (Option == 3 -> suiteMenu(ProjectId, SuiteId));
+    (write(invalid_option), suiteMenu(ProjectId, SuiteId))).
+
+menuChangeStatus(ProjectId, SuiteId, CaseId) :-
+    constants:test_case_header(CaseHeader),
+    writeln(CaseHeader),
+    writeln('- Editando Caso de Testes'),
+    writeln('Estado atual'),
+    listCase(ProjectId, SuiteId, CaseId), nl,
+    writeln('-- Alterar status do Caso de Testes'),
+    writeln('(1) Passou'),
+    writeln('(2) Nao passou'),
+    writeln('(3) Erro na execucao'),
+    writeln('(4) Voltar'),
+    readNumber(Option),
+    (((Option == 1 -> NewStatus = 'Passou');
+    (Option == 2 -> NewStatus = 'Nao passou');
+    (Option == 3 -> NewStatus = 'Erro na execucao')),
+    testCase(ProjectId, SuiteId, CaseId, Name, Goal, OldStatus, Preconditions),
+    retract(testCase(ProjectId, SuiteId, CaseId, Name, Goal, OldStatus, Preconditions)),
+    assertz(testCase(ProjectId, SuiteId, CaseId, Name, Goal, NewStatus, Preconditions)));
+    (write('Opcao invalida.'), menuChangeStatus(ProjectId, SuiteId, CaseId)),
+    suiteMenu(ProjectId, SuiteId).
+
+deleteTestCase(ProjectId, SuiteId) :-
+    constants:test_case_header(CaseHeader),
+    writeln(CaseHeader),
+    writeln('Informe o id do caso de teste: '),
+    readNumber(CaseId),
+    (validCaseId(CaseId) -> listCase(ProjectId, SuiteId, CaseId), write('Tem certeza que deseja excluir esse caso de testes? ((1)Sim/(2)Nao)'), nl,
+    readNumber(Option),
+    (((Option == 1) -> retract(testCase(ProjetId, SuiteId, CaseId, _, _, _, _)), writeln('Caso de testes excluido com sucesso.'));
+    (writeln('Caso de testes nao excluido.'))),
+    suiteMenu(ProjectId, SuiteId));
+    writeln('Id informado nao cadastrado.'),
+    writeln('Pressione qualquer tecla para continuar...'),
+    get_char(_),
+    suiteMenu(ProjectId, SuiteId).
