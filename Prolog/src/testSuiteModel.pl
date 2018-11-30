4c2a5a5ad9d6c5fb70c3f55dce96460014ba0ddb
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2018-11-30 12:50:43 -0300

    Id de suite automático

--- Prolog/src/testSuiteModel.pl
+++ Prolog/src/testSuiteModel.pl
@@ -4,11 +4,34 @@
 :- use_module(utils).
 :- use_module(constants).
 
-/*Suite (id, nome, descrição, idProjeto)  */
-:- dynamic(suite/4).
+:- dynamic suite/4, nextId/2.
 
-loadSuites():- readSuiteFromFile.
+loadSuites():- readSuiteFromFile,
+                readNextIdFromFile.
 
+saveNextId():-utils:createDirectory('data'),
+            open('data/next_id_suite.dat', write, InStream),
+            forall(nextId(Id, Projeto), 
+            (writeln(InStream, Id),
+            writeln(InStream, Projeto))),
+            close(InStream).
+
+readNextIdFromFile():-
+            exists_file('data/next_id_suite.dat') ->(
+            open('data/next_id_suite.dat', read, InStream),
+            readNextId(InStream),
+            close(InStream));
+            true.
+
+readNextId(InStream):- at_end_of_stream(InStream).
+readNextId(InStream):- utils:readLine(InStream, Id),
+            utils:readLine(InStream, Projeto),
+            string_to_atom(Id, AtomId),
+            atom_number(AtomId, NumberId),
+            string_to_atom(Projeto, AtomProjeto),
+            atom_number(AtomProjeto, NumberProjeto),
+            assertz(nextId(NumberId, NumberProjeto)),
+            readSuite(InStream).
 
 saveTestSuite():- utils:createDirectory('data'),
                     open('data/test_suite.dat', write, InStream),
