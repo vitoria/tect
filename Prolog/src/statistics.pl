d57674f6228af01acee7ea72fbcbdb12baef142c
joeberth <joeberth.souza@ccc.ufcg.edu.br>
2018-11-30 04:03:45 -0300

    Indicando os modulos

--- Prolog/src/statistics.pl
+++ Prolog/src/statistics.pl
@@ -18,13 +18,13 @@ readNumber(Number):-
 sumStatisticsProject(_,[], Result):- Result is 0.
 sumStatisticsProject(ProjectId,[IdSuite|List], Result) :-  
                     sumStatisticsProject(ProjectId,List, Result2),
-                    testCase:calculateStatiscs(ProjectId, IdSuite, StatSuite), Result is Result2 + StatSuite.
+                    model:testCase:calculateStatiscs(ProjectId, IdSuite, StatSuite), Result is Result2 + StatSuite.
 
 
 getSuitesList(Projeto, X):- 
-    findall(I, testSuite:suite(I, _, _, Projeto), X). 
+    findall(I, model:testSuite:suite(I, _, _, Projeto), X). 
 getAllSuites(Projeto, X):- 
-    findall([I,J,K,Projeto],project(I, J, K, Projeto), X). 
+    findall([I,J,K,Projeto],model:project:project(I, J, K, Projeto), X). 
 
 getNumberOfSuites(Count) :- 
     aggregate_all(count, testSuite:suite(_, _, _, _), Count).
@@ -50,9 +50,9 @@ getProjectsStatistics([ProjectsTouple]) :-
 
 
 getProjectIdList(X):- 
-    findall(I, project:project(I, _, _, _), X). 
+    findall(I, model:project:project(I, _, _, _), X). 
 getProjectIdToupleList(X):- 
-    findall([I,J], project:project(I, J, _, _), X). 
+    findall([I,J], model:project:project(I, J, _, _), X). 
 
 chooseStatisticsAction(Option) :-
     getProjectIdList(Result),
@@ -82,7 +82,7 @@ statisticsFromAProject(ProjectsId):-
 generateStatisticsString(_,[]).
 generateStatisticsString(ProjectId,[Id, Name,_,_|Suites]):-
         write(Id), write(" - "), write(Name), write(" - "), 
-        testCase:calculateStatiscs(ProjectId, Id, StatSuite), write(StatSuite),nl,
+        model:testCase:calculateStatiscs(ProjectId, Id, StatSuite), write(StatSuite),nl,
         generateStatisticsString(ProjectId, Suites).
 
 
@@ -96,6 +96,6 @@ statisticsMenu(LoggedUser) :-
     write("Informe a opção desejada: "),
     readNumber(Entrada),
     (isOptionValidStat(Entrada) ->
-           (Entrada =:= 3 -> write("Retornando ao menu anterior..."),util:systemPause(), mainSystem:systemMenu(LoggedUser);
+           (Entrada =:= 3 -> write("Retornando ao menu anterior..."),model:util:systemPause(), model:mainSystem:systemMenu(LoggedUser);
                                 (tty_clear, chooseStatisticsAction(Entrada))));                
-    write("Opção Inválida"), util:systemPause(), statisticsMenu(LoggedUser).
+    write("Opção Inválida"), model:util:systemPause(), statisticsMenu(LoggedUser).
